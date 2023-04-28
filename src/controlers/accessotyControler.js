4cf870496b6f14d0deb1672447e83eacdb9dac52
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-04-28 18:35:05 +0300

    create accessory functionality

--- src/controlers/accessotyControler.js
+++ src/controlers/accessotyControler.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 
+const Accessory = require('../models/Accessory')
 //URL: /accessory/create
 
 router.get('/create', (req, res) => {
     res.render('accessory/createAccessory')
 });
 
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
 
+    await Accessory.create({ name, description, imageUrl });
+
+    res.redirect('/');
+});
 
 
 
